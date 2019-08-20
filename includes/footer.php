d9c908b3bb476b28a486dab79963285e25c0603c
GuilhermeH6 <guilhermeholanda870@gmail.com>
2019-08-20 15:22:53 -0300

    GH-13 Incluindo o Footer

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<script type="text/javascript" src="js/materialize.min.js"></script>
+<script>
+        M.AutoInit();
+</script>
+
+<footer style="background-color: white;"class="page-footer">
+  <div style="background-color: black;"class="footer-copyright">
+            <div class="container">
+            © 2019 Moviemakers Copyright
+            <a class="white-text text-lighten-4 right" href="#!">Termos de uso</a>
+            <a class="white-text text-lighten-4 right" href="#!">Privacidade e cookies</a>
+            </div>
+          </div>
+        </footer>
+        
+</body>
+</html>

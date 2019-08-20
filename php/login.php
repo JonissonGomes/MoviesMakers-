d90e333e27a32a6171a9adb1bf9d047580a8bfd8
JonissonGomes <42873530+JonissonGomes@users.noreply.github.com>
2019-08-20 14:17:52 -0300

    Adição de um cabeçalho

--- php/login.php
+++ php/login.php
@@ -7,6 +7,13 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </head>
 <body>
-
+<!-- NavBar -->
+	 <nav>
+    <div style="background-color:#006064;" class="nav-wrapper">
+    <div class="center-align">
+      <a href="index.php" class="center hide-on-med-and-down">M O V I E M A K E R S</a>
+  </div>
+    </div>
+  </nav>
 </body>
 </html>

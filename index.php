44a6569b38bcdd37245431ceea56acf0a5db6db0
JonissonGomes <42873530+JonissonGomes@users.noreply.github.com>
2019-08-20 07:33:12 -0300

    Importação das bibliotecas do Materialize
    (Links das bibliotecas online do Framework utilizado).

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Moviemakers</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </head>
 <body>
 	

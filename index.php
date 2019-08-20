55ad02b74ea590ce0e621125333df06b29654486
JonissonGomes <42873530+JonissonGomes@users.noreply.github.com>
2019-08-20 07:36:14 -0300

    Criação da barra de navegação

--- index.php
+++ index.php
@@ -7,6 +7,17 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </head>
 <body>
+	<!-- NavBar -->
+	<nav>
+    <div style="background-color:#006064;" class="nav-wrapper">
+    <div class="center-align">
+      <a href="index.php" class="left">M O V I E M A K E R S</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="login.php" class="waves-effect waves-light btn">Entrar</a></li>
+      </ul>
+  </div>
+    </div>
+  </nav>
 	
 </body>
 </html>

ace285741cee0ba21e2f9992fd74efaebf9c6f0e
lipeh777 <filipeizidorio@outlook.com>
2019-08-20 15:54:44 -0300

    GH-14 arquivo de conexão com o banco de dados em PDO

---
+++ php/connect.php
@@ -0,0 +1,22 @@
+<?php 
+
+function getConnection(){
+
+	$host = "localhost";
+	$bd = "web20191filmes";
+	$usr = "root";
+	$pass = "ifpe";
+	//essa senha depende de qual maquina vc ta acessando o bd 
+
+	try{
+		$connect = new PDO("mysql:host =".$host.";dbname =".$bd, $usr, $pass);
+		return $connect;
+	} catch(PDOException $e){
+		return "Deu merda e:<br>" . $e->getMessage();
+	}
+
+}
+
+$conexao = getConnection();
+
+ ?>

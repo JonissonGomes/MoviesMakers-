4115fa27a943c7c0393383c4ef45209f4ebaa30f
lipeh777 <filipeizidorio@outlook.com>
2019-08-20 22:07:33 -0300

    GH-13 adicionando link para o arquivo css personalizado, style.css

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,7 @@
 	<title>Entrar na minha conta</title>
 
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<link rel="stylesheet" type="text/css" href="../materialize/css/style.css">
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 

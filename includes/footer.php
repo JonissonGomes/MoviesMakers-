8ba47678da57a6545aa57e508e4cbcbc729100b5
K0rgana <anagrom1999@gmail.com>
2019-08-22 20:34:54 -0300

    GH-13 corrigindo o footer incompleto

--- includes/footer.php
+++ includes/footer.php
@@ -3,15 +3,32 @@
         M.AutoInit();
 </script>
 
-<footer style="background-color: white;"class="page-footer">
-  <div style="background-color: black;"class="footer-copyright">
-            <div class="container">
-            © 2019 Moviemakers Copyright
-            <a class="white-text text-lighten-4 right" href="#!">Termos de uso</a>
-            <a class="white-text text-lighten-4 right" href="#!">Privacidade e cookies</a>
-            </div>
-          </div>
-        </footer>
+<!-- Finalização do site -->
+<footer class="page-footer">
+	<div class="container">
+  	<div class="row">
+    	<div class="col l6 s12">
+      	<h5 class="white-text">Moviemakers</h5>
+      	<p class="grey-text text-lighten-4">Você pode entrar em contato conosco utlizando os recursos listados:</p>
+    	</div>
+    	<div class="col l4 offset-l2 s12">
+      	<h5 class="white-text">Links</h5>
+      	<ul>
+        	<li><a class="grey-text text-lighten-3" href="cadastro.php">Crie sua conta grátis</a></li>
+        	<li><a class="grey-text text-lighten-3" href="#">Fale conosco</a></li>
+        	<li><a class="grey-text text-lighten-3" href="#!">Perguntas recentes</a></li>
+        	<li><a class="grey-text text-lighten-3" href="sobre.php">Sobre</a></li>
+      	</ul>
+    	</div>
+  	</div>
+	</div>
+	<div style="background-color: black;" class="footer-copyright">
+  	<div class="container">
+  	© 2019 Moviemakers Copyright
+  	<a class="white-text text-lighten-4 right" href="#!">Suporte</a>
+  	</div>
+	</div>
+</footer>
         
 </body>
 </html>

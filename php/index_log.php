7d26979926b46849242e2c585fdfacd607984bf4
AlissonThyago <alissonthiagoamorim@gmail.com>
2019-08-28 13:43:17 -0300

    GH-7 adicionando consulta para retornar a lista priorizada

--- php/index_log.php
+++ php/index_log.php
@@ -1,4 +1,5 @@
 <?php 
+	include_once'action/connect.php'; 
 	include_once'../includes/header.php';
 ?>
 
@@ -9,6 +10,24 @@
 			</div>
 		</div>
 	</nav>
+
+	<!-- seleciona os filmes ordenados pela quantidade de pessoas que os adicionaram -->
+	<?php
+		$sql = $conexao -> prepare("SELECT *, (select count(*) FROM users_movies WHERE movie_id = movies.id) count_users FROM movies order by count_users desc, name");
+		$sql -> execute();
+		$dados_us_mv = $sql -> fetch(PDO::FETCH_ASSOC);
+		// var_dump($dados_us_mv);
+	?>
+
+<!-- 	<?php foreach ($dados_us_mv as $dados_users_movies): ?>
+		<?php echo $dados_users_movies."<br/>"; ?>
+	<?php endforeach ?>
+
+	<?php foreach ($dados_us_mv as $dados_users_movies): ?>
+		<tr>
+			<td><?= $dados_users_movies ?></td>
+		</tr>
+	<?php endforeach ?> -->
 	
 	<div class="section">
 		<div class="row">
@@ -25,7 +44,7 @@
 				<div class="carousel carousel-slider center">
 					<?php foreach ($dados_us_mv as $dados_users_movies): ?>
 						<div class="carousel-item #607d8b blue-grey white-text" href="#one!">
-							<h2></h2>
+							<h2><?= $dados_users_movies ?></h2>
 							<p class="white-text"></p>
 						</div>
 					<?php endforeach ?>
@@ -36,6 +55,23 @@
 
 	<hr>
 
+	<!-- lista todos os filmes, priorizando os que o usuário 2 já assistiu, seguido pelos não assistidos, seguido pelos não marcados -->
+	<?php 
+		$sqli = $conexao -> prepare("SELECT movies.*, users_movies.watched watched FROM movies left join users_movies on users_movies.movie_id = movies.id and users_movies.user_id = 2 order by coalesce(users_movies.watched, 2), movies.name");
+		$sqli -> execute();
+		$dados_mv = $sqli -> fetch(PDO::FETCH_ASSOC);
+		// var_dump($dados_mv);
+	 ?>
+
+<!-- 	<?php foreach ($dados_mv as $dados_movies): ?>
+		<?php echo $dados_movies."<br/>"; ?>
+	<?php endforeach ?>
+
+	<?php foreach ($dados_mv as $dados_movies): ?>
+		<tr>
+			<td><?= $dados_movies ?></td>
+		</tr>
+	<?php endforeach ?> -->
 	<div class="section">
 		<div class="row">
 			<br>
@@ -51,7 +87,7 @@
 				<div class="carousel carousel-slider center">
 					<?php foreach ($dados_mv as $dados_movies): ?>
 						<div class="carousel-item #607d8b blue-grey white-text" href="#one!">
-							<h2></h2>
+							<h2><?= $dados_movies ?></h2>
 							<p class="white-text"></p>
 						</div>
 					<?php endforeach ?>
